2d96834ffa4316185f76339ea5215fbd012d5614
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-24 11:47:00 +0200

    chore: Add GetSudggest endpoint for retrieving song suggestions

--- sonkBackApp/api/urls.py
+++ sonkBackApp/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SearchMusic,GetArtist,GetSong
+from .views import SearchMusic,GetArtist,GetSong, GetSudggest
 
 urlpatterns = [
     path('search/', SearchMusic.as_view(), name='search'),
     path('artist/',GetArtist.as_view(),name='artist' ),
-    path('song/',GetSong.as_view(),name='song' )
+    path('song/',GetSong.as_view(),name='song'),
+    path('getSuggestions/',GetSudggest.as_view(),name='getSuggestions')
 ]
