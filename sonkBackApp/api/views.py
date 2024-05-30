0ce246ea97dfdd8ce9a566ac41eabaaf34a14307
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-30 11:48:50 +0200

    chore: Refactor GetPosdscats endpoint to filter albums in search results

--- sonkBackApp/api/views.py
+++ sonkBackApp/api/views.py
@@ -205,12 +205,13 @@ class GetPosdscats(APIView):
             search = request.query_params
             query = search.get('query')
             try:
-                data = ytmusicapi.search(query=query , limit=100)
-                
-                newData = []
+                data = ytmusicapi.search(query=query , limit=100, filter='albums')
+                newData=[]
+                for item in data:
+                    if item["resultType"] != 'song':
+                        newData.append(item)    
             
-                        
-                return Response(data, status=status.HTTP_200_OK)
+                return Response(newData, status=status.HTTP_200_OK)
             
             except Exception as e:
                 return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
