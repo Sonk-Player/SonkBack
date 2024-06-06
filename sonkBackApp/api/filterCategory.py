203389189dbdb8ddffc0ec1f703bfa4e5c1896f2
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-06-06 11:22:34 +0200

    Update filters

--- sonkBackApp/api/filterCategory.py
+++ sonkBackApp/api/filterCategory.py
@@ -2,7 +2,7 @@ class filterCategory:
     
         
     def verfiyFilters( filter: str): 
-        filters = ["songs", "videos", "albums", "artists", "playlists", "community_playlists", "featured_playlists", "uploads"]
+        filters = ["songs", "videos", "albums", "artists", "playlists", "community_playlists", "featured_playlists", "uploads", "profiles", "podcasts", "episodes"]
         try:
             if filter in  filters:
                 return True
